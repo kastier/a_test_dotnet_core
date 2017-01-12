c491a36f7ba177276e9676d6202adc7e19cd6c2a
haste <haste@HASTE-16>
2017-01-12 14:29:45 +0800

    modified:   ../dotnet_core_test_v3.sln 	new file:   prac/Person.cs 	new file:   prac/Program.cs 	new file:   prac/Properties/AssemblyInfo.cs 	new file:   prac/prac.xproj 	new file:   prac/project.json

---
+++ dotnet_core_test_v3/src/prac/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prac
+{
+    public class Person
+    {
+        private string _name;
+        public string Name
+        {
+            get { Console.WriteLine("汉字"); return _name+"--weiba"; }
+            set { _name = value; }
+        }
+        public int Age { get; set; }
+
+    }
+}
---
+++ dotnet_core_test_v3/src/prac/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prac
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person p = new Person();
+            Console.WriteLine(p.Name);
+            p.Name = "hi";
+            Console.WriteLine(p.Name);
+        }
+    }
+}
